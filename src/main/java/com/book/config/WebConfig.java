ec97c12ef0169efe2bbc08e8b7da425c608c8405
RibinRaju <ribinraju2000@gmail.com>
2024-12-13 04:22:29 +0530

    neworginconnector

--- src/main/java/com/book/config/WebConfig.java
+++ src/main/java/com/book/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") 
-                .allowedOrigins("*") 
+                .allowedOrigins("http://localhost:5173","https://book-management-frontend-xi.vercel.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); 
